1ece37308e415a9b95a7dfb77e339077f70a1539
AlexHeckendorn <alex.heckendorn@gmail.com>
2023-03-30 14:56:27 -0600

    Adding directories, alongside the default apps when creating quickstart archetype

---
+++ src/main/java/org/epam/training/Alexander_Heckendorn/App.java
@@ -0,0 +1,13 @@
+package org.epam.training.Alexander_Heckendorn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
