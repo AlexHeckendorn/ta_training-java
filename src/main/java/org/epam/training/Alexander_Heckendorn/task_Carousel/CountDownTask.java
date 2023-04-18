5646143d84dc89857b484b1762e5740b94928ec9
AlexHeckendorn <alex.heckendorn@gmail.com>
2023-04-18 11:25:17 -0600

    Adding carousel to project

---
+++ src/main/java/org/epam/training/Alexander_Heckendorn/task_Carousel/CountDownTask.java
@@ -0,0 +1,27 @@
+package org.epam.training.Alexander_Heckendorn.task_Carousel;
+
+public class CountDownTask implements Task{
+    private int value;
+
+    public CountDownTask(int value) {
+        this.value = Math.max(value, 0);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public void execute() {
+        if(value > 0){
+            value--;
+        }
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return value == 0;
+
+    }
+}
